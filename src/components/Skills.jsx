b3c061cc964a2cf650c989b5ae2df28eb93ac8d3
Kevin <wangkw523@gmail.com>
2024-12-09 15:29:30 +0000

    Add skills and constants

---
+++ src/components/Skills.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { skills } from "../constants"
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-gray-950 text-gray-300'>
+    
+        {/* Container */}
+        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+            <div>
+                <p className="text-4xl font-bold inline border-b-4 border-green-700">Skills</p>
+                <p className="py-4">These are the technologies I've worked with</p>
+            </div>
+
+            {/* Cards */}
+            <ul className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+                {skills.map((item, index) => (
+                    <li className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className='w-20 mx-auto' src={item.image} alt={item.skill + " icon"} />
+                        <p className='my-4'>{item.skill}</p>
+                    </li>
+                ))}
+            </ul>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Skills
---
+++ src/constants/index.jsx
@@ -0,0 +1,21 @@
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import FireBase from '../assets/firebase.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
+
+export const skills = [
+    {skill: "HTML", image: HTML},
+    {skill: "CSS", image: CSS},
+    {skill: "JAVASCRIPT", image: JavaScript},
+    {skill: "REACT", image: ReactImg},
+    {skill: "GITHUB", image: GitHub},
+    {skill: "NODE JS", image: Node},
+    {skill: "MONGO DB", image: Mongo},
+    {skill: "TAILWIND", image: Tailwind},
+    {skill: "FIREBASE", image: FireBase},
+]

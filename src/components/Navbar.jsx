c2cd0fb87678d94edfd486318264c45d64855843
KaiWhen <wangkw523@gmail.com>
2024-12-16 09:19:46 +0000

    idk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-gray-950 text-gray-300'>
         <div>
             <img className='rounded-full' src={Logo} alt="Logo" style={{width: '50px'}} />
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,7 @@ const Skills = () => {
 
             {/* Cards */}
             <ul className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-                {skills.map((item, index) => (
+                {skills.map((item, _) => (
                     <li className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
                         <img className='w-20 mx-auto' src={item.image} alt={item.skill + " icon"} />
                         <p className='my-4'>{item.skill}</p>

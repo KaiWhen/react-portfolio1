dd3384d67a39ef705c082aa7ffcf8c0712f5fcdc
KaiWhen <wangkw523@gmail.com>
2024-12-24 14:05:19 +0000

    Extend skills y padding

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
             </div>
 
             {/* Cards */}
-            <ul className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+            <ul className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-20">
                 {skills.map((item, i) => (
                     <StaggerReveal i={i}>
                     <li className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">

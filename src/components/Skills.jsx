17943e311ce89352c713b56782f4030c2f1b3be5
KaiWhen <wangkw523@gmail.com>
2024-12-24 14:25:31 +0000

    Make skills list scrollable on mobile

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,22 +10,24 @@ const Skills = () => {
         {/* Container */}
         <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
             <Reveal>
-            <div>
-                <p className="text-4xl font-bold inline border-b-4 border-green-700">Skills</p>
-                <p className="py-4">These are the technologies I've worked with</p>
-            </div>
+                <div>
+                    <p className="text-4xl font-bold inline border-b-4 border-green-700">Skills</p>
+                    <p className="py-4">These are the technologies I've worked with</p>
+                </div>
 
-            {/* Cards */}
-            <ul className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-20">
-                {skills.map((item, i) => (
-                    <StaggerReveal i={i}>
-                    <li className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className='w-20 mx-auto' src={item.image} alt={item.skill + " icon"} />
-                        <p className='my-4'>{item.skill}</p>
-                    </li>
-                    </StaggerReveal>
-                ))}
-            </ul>
+                {/* Cards */}
+                <div className="w-full h-full max-h-[70vh] overflow-y-auto sm:overflow-y-visible scrollbar-thin scrollbar-thumb-green-700 scrollbar-track-gray-800">
+                    <ul className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+                        {skills.map((item, i) => (
+                            <StaggerReveal i={i}>
+                            <li className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                                <img className='w-20 mx-auto' src={item.image} alt={item.skill + " icon"} />
+                                <p className='my-4'>{item.skill}</p>
+                            </li>
+                            </StaggerReveal>
+                        ))}
+                    </ul>
+                </div>
             </Reveal>
         </div>
 
